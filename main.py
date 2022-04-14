006c3d0207bac8756c63898be5ab3a92a9f5cdba
Arek123312 <50328149+Arek123312@users.noreply.github.com>
2022-04-14 21:50:34 +0200

    Adding new collections to json file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from solana.rpc.api import Client
 from solana.publickey import PublicKey
 import json
+from os import path
 
 file_path = "D:\MagicEdenFloorTracker\collections.json"
 
@@ -12,13 +13,27 @@ publicKey = 'ALcLZyR74AZVc6p1SJnoEyLHeViKtdtRj5xHKvsaRYzF'
 #collections = ["astrals", "famous_fox_federation", "tombstoned", "tombstoned", "yaku_corp_capsulex", "yaku_corp_capsulex",
 #"yaku_corp", "yaku_corp"]
 
-f = open(file_path, 'r')
+f = open(file_path, 'r+')
 data = json.load(f)
 
 menu_choice = input("1. Dodaj NFT do portfolio\n2. Usuń NFT z portfolio\n3. Wyświetl aktualne portfolio\n4. Oblicz wartość portfolio\n")
 
 match(menu_choice):
     case '1':
+        listObj = []
+
+        addName = input("Wprowadź nazwę kolekcji (np. famous_fox_federation): ")
+        addAmount = int(input("Wprowadź ilość sztuk z kolekcji: "))
+
+        new_data = {"Name":addName,
+                    "Amount":addAmount
+                    }
+
+        data["Collections"].append(new_data)
+        # Sets file's current position at offset.
+        f.seek(0)
+        # convert back to json.
+        json.dump(data, f, indent = 4)
         
     case '3':
         for i in data['Collections']:
