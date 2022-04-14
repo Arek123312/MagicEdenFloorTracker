3cb96941f4e26b3f7d97b7d3e46a561b9bef778d
Arek123312 <50328149+Arek123312@users.noreply.github.com>
2022-04-14 18:47:34 +0200

    Check floor based on input

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import requests
 
 LAMPORTS = 1000000000 # number of lamports in 1 solana
 
-url = "http://api-mainnet.magiceden.dev/v2/collections/degods/stats"
+collection = str(input('Wprowadź nazwę kolekcji: '))
+
+url = "http://api-mainnet.magiceden.dev/v2/collections/" + collection + "/stats"
 
 payload={}
 headers = {}
 
 response = requests.request("GET", url, headers=headers, data=payload).json()
 
+print("Floor: ", end='') 
 print(response['floorPrice']/LAMPORTS)
 
-#commit
 
